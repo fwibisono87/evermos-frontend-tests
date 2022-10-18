8564b8a26abc10cb4a73f5b6667421679a553aeb
Francis Wibisono <francis.wibisono@ui.ac.id>
2022-10-18 23:43:56 +0700

    feat: productCard

---
+++ src/components/Tag.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let title = 'Hoshimachi Suisei';
+</script>
+
+<template>
+	<div class="h-min w-min py-1 px-2 rounded-lg bg-orange-300">
+		<span class="text-[0.75rem] whitespace-nowrap">
+			{title}
+		</span>
+	</div>
+</template>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="bg-slate-500 min-h-screen w-screen">
+  <div class="flex flex-row shadow-md px-4 py-4 bg-primary ">
+    <a href="/">
+      <h1 class="text-3xl font-semibold text-slate-300 select-none">Catalogue</h1>
+    </a>
+  </div>
+  <div class="flex flex-col p-4 " >
+      <slot />
+  </div>
+</div>
